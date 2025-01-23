ba4334da83feb1a3ce81e2db76861d358ecf0814
Yupi <youngperchik2404@gmail.com>
2025-01-23 15:21:23 +0500

    Finished conditions and edited output

--- src/Main.java
+++ src/Main.java
@@ -13,40 +13,61 @@ public class Main {
 
 
         do {
-            System.out.println("Команды:");
+            System.out.println("Команды:\n");
             System.out.println("1. Add (Добавить новую задачу)\n2. Get (Показать задачу с наибольшим приоритетом)\n3. Done (Удаляет задачу)\n4. Exit (Выход)\n");
 
-            System.out.print("Выберите одну из команд, чтобы выполнить действие: ");
+            System.out.print("\nВыберите одну из команд, чтобы выполнить действие: ");
             int chooseCommand = input.nextInt();
 
             if (chooseCommand == 1)
             {
                 System.out.print("Введите название новой задачи: ");
                 input.nextLine();
-                String discription = input.next();
+                String description = input.next();
 
                 System.out.print("Введите приоритет новой задачи (От 0 до 9): ");
                 int priority = input.nextInt();
 
-                tasks.put(priority, discription);
+                tasks.put(priority, description);
             }
             else if (chooseCommand == 2)
             {
                 Map.Entry<Integer, String> lastTask = tasks.lastEntry();
-                System.out.println("Задача с наибольшим приоритетом: " + lastTask.getValue());
+                System.out.println("\n\nЗадача с наибольшим приоритетом: " + lastTask.getValue() + " ("+ (lastTask.getKey()) + ")\n\n");
             }
             else if (chooseCommand == 3)
             {
-                // do
+                System.out.println("\n\nВесь список задач:\n");
+
+                Set<Integer> keys = tasks.keySet();
+                System.out.println("Приоритеты задач -> " + keys);
+
+                Collection <String> values = tasks.values();
+                System.out.println("Задачи -> " + values);
+
+                System.out.println("Выберите приоритет задачи, чтобы удалить её");
+
+                System.out.print("\nВведите приоритет: ");
+                int priorityToDelete = input.nextInt();
+
+                tasks.remove(priorityToDelete);
+                System.out.println("\n\nЗадача успешно удалена!\n\n");
             }
             else if (chooseCommand == 4)
             {
                 System.out.println("\n\nВесь список задач:\n");
+
                 Set<Integer> keys = tasks.keySet();
                 System.out.println("Приоритеты задач -> " + keys);
+
                 Collection <String> values = tasks.values();
                 System.out.println("Задачи -> " + values);
+
                 break;
+            }
+            else
+            {
+                System.out.println("\n\nОшибка ввода!!");
             }
         }while(true);
     }
