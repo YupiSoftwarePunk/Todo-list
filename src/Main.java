4e318c6e4642535f2bbc27557de2e726f3df1c44
Yupi <youngperchik2404@gmail.com>
2025-01-23 14:24:46 +0500

    edited condition and added cycle

--- src/Main.java
+++ src/Main.java
@@ -14,27 +14,37 @@ public class Main {
         tasks.put(1, "Купить носки");
 
 
-        System.out.println("Команды:");
-        System.out.println("1. Add (Добавить новую задачу)\n2. Get (Показать задачу с наибольшим приоритетом)\n3. Done (Удаляет задачу)\n4. Exit (Выход)\n");
+        do {
+            System.out.println("Команды:");
+            System.out.println("1. Add (Добавить новую задачу)\n2. Get (Показать задачу с наибольшим приоритетом)\n3. Done (Удаляет задачу)\n4. Exit (Выход)\n");
 
-        System.out.print("Выберите одну из команд, чтобы выполнить действие: ");
-        int chooseCommand = input.nextInt();
+            System.out.print("Выберите одну из команд, чтобы выполнить действие: ");
+            int chooseCommand = input.nextInt();
 
-        if (chooseCommand == 1)
-        {
-            System.out.println("Введите название новой задачи: ");
-            input.nextLine();
-            String discription = input.next();
+            if (chooseCommand == 1)
+            {
+                System.out.print("Введите название новой задачи: ");
+                input.nextLine();
+                String discription = input.next();
 
-            System.out.print("Введите приоритет новой задачи (От 0 до 9): ");
-            int priority = input.nextInt();
+                System.out.print("Введите приоритет новой задачи (От 0 до 9): ");
+                int priority = input.nextInt();
 
-            tasks.put(priority, discription);
-        }
-        else if (chooseCommand == 2)
-        {
-            Map.Entry<Integer, String> lastTask = tasks.lastEntry();
-            System.out.println("Задача с наибольшим приоритетом: " + lastTask.getValue());
-        }
+                tasks.put(priority, discription);
+            }
+            else if (chooseCommand == 2)
+            {
+                Map.Entry<Integer, String> lastTask = tasks.lastEntry();
+                System.out.println("Задача с наибольшим приоритетом: " + lastTask.getValue());
+            }
+            else if (chooseCommand == 3)
+            {
+
+            }
+            else if (chooseCommand == 4)
+            {
+                break;
+            }
+        }while(true);
     }
 }
